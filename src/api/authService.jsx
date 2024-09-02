354734367b352745618ebd3849d33affea0bd58a
Adrian Iurian <adrian.iurian1@gmail.com>
2024-09-02 23:37:02 +0300

    feat: create useFetch hook

--- src/api/authService.jsx
+++ src/api/authService.jsx
@@ -1,38 +1,29 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
-// const BASE_URL = "http://pd-structuri.ro:8081";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://pd-structuri.ro:8081";
 
 const API_URL = `${BASE_URL}/api/v1/auth`;
 
 export const register = async (userData) => {
-    console.log(`${API_URL}/register`);
-    console.log(userData);
-    const response = await axios.post(`${API_URL}/register`, userData);
-    console.log("register");
-    console.log(response);
-    return response.data;
+    return await axios.post(`${API_URL}/register`, userData);
 };
 
 export const login = async (credentials) => {
-    const response = await axios.post(`${API_URL}/login`, credentials);
-    return response.data;
+    return await axios.post(`${API_URL}/login`, credentials);
 };
 
 export const sendResetPasswordEmail = async (email) => {
-    const response = await axios.post(`${API_URL}/reset-password`, { email });
-    return response.data;
+    return await axios.post(`${API_URL}/reset-password`, { email });
 };
 
 export const resetPassword = async (token, password) => {
-    const response = await axios.put(`${API_URL}/reset-password`, {
+    return await axios.put(`${API_URL}/reset-password`, {
         token,
         password,
     });
-    return response.data;
 };
 
 export const enableUser = async (token) => {
-    const response = await axios.put(`${API_URL}/enable/${token}`);
-    return response.data;
+    return await axios.put(`${API_URL}/enable/${token}`);
 };
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (fetchFn, initialValue, ...params) => {
+    const [data, setData] = useState(initialValue);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetchFn(...params);
+                const jsonData = response.data;
+                setData(jsonData);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [fetchFn, params]);
+
+    return { data, setData, loading: isLoading, error };
+};
+
+export default useFetch;
