2716d98ef0e9588b0689c08f52aa3ace3b013949
Victor Salajan <victorsalajan29@gmail.com>
2024-09-10 13:09:54 +0300

    feat: configured calendar post layout

--- _Presentation-Website/src/app/calendar/calendar.component.ts
+++ _Presentation-Website/src/app/calendar/calendar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
     imports: [
-        NavbarComponent
+        NavbarComponent,
+        MatIcon
     ],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
