fdaeec9c1adc046117ff206f6fb92ae380a9ba66
Victor Salajan <victorsalajan29@gmail.com>
2024-09-11 13:24:07 +0300

    added photo to team card

--- _Presentation-Website/src/app/echipa/echipa.component.ts
+++ _Presentation-Website/src/app/echipa/echipa.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-echipa',
   standalone: true,
     imports: [
         NavbarComponent,
-        NgIf
+        NgIf,
+        NgForOf
     ],
   templateUrl: './echipa.component.html',
   styleUrls: ['../app.component.css', './echipa.component.css']
 })
 export class EchipaComponent {
-
+    organizerNameList = [
+        'Gabi Mircea',
+        'Jane Smith'
+    ];
+    memberNameList = [
+        'cineva ajerwuw',
+        'no one'
+    ];
 }
